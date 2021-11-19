ff3b786317c4009d2b52d148486388383a14add1
patrickshyee <patrickshyee@gmail.com>
2021-11-18 18:51:04 -0700

    Doing the tutorial for ReactJS from https://reactjs.net/tutorials/aspnetcore.html with the FAQ page.

---
+++ Controllers/VehiclesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace React_AggressionFabrications.Controllers
+{
+    public class VehiclesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Http;
+using JavaScriptEngineSwitcher.V8;
+using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
+using React.AspNet;
 using Microsoft.EntityFrameworkCore;
 using React_AggressionFabrications.Data;
 
@@ -12,6 +16,11 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddReact();
+
+// A JS engine is registered
+builder.Services.AddJsEngineSwitcher(options => options.DefaultEngineName = V8JsEngine.EngineName).AddV8();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -29,6 +38,27 @@ else
 }
 
 app.UseHttpsRedirection();
+
+// Initialise ReactJS.NET. Must be before static files.
+app.UseReact(config =>
+{
+    // If you want to use server-side rendering of React components,
+    // add all the necessary JavaScript files here. This includes
+    // your components as well as all of their dependencies.
+    // See http://reactjs.net/ for more information. Example:
+    //config
+    //  .AddScript("~/js/First.jsx")
+    //  .AddScript("~/js/Second.jsx");
+
+    // If you use an external build too (for example, Babel, Webpack,
+    // Browserify or Gulp), you can improve performance by disabling
+    // ReactJS.NET's version of Babel and loading the pre-transpiled
+    // scripts. Example:
+    //config
+    //  .SetLoadBabel(false)
+    //  .AddScriptWithoutTransform("~/js/bundle.server.js");
+});
+
 app.UseStaticFiles();
 
 app.UseRouting();
